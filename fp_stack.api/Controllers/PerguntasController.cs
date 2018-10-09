58916644ded031d734e5108d26a62656446bda86
Jean A. Sena <jadsena@hotmail.com>
2018-10-08 22:54:35 -0300

    Aula 3

---
+++ fp_stack.api/Controllers/PerguntasController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using fp_stack.core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fp_stack.api.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{apiVersion}/[controller]")]
+    [ApiController]
+    public class PerguntasController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public PerguntasController(Context context)
+        {
+            _context = context;
+        }
+
+        //public List<Pergunta> Index()
+        //{
+        //    return _context.Perguntas.ToList();
+        //}
+        //[HttpGet]
+        //[ProducesResponseType(200,Type =typeof(List<Pergunta>))]
+        //[ProducesResponseType(400)]
+        //public IActionResult Index2()
+        //{
+        //    var perguntas = _context.Perguntas.ToList();
+        //    if (perguntas.Count() == 3)
+        //        return BadRequest();
+        //    return Ok(perguntas);
+        //}
+        [HttpGet]
+        //[Route("")]
+        public ActionResult<List<Pergunta>> Get()
+        {
+            var perguntas = _context.Perguntas.ToList();
+            return perguntas;
+        }
+        //[HttpGet]
+        //[Route("{id}")]
+        //public ActionResult<Pergunta> Get(int id)
+        //{
+        //    var pergunta = _context.Perguntas.FirstOrDefault;
+        //    return pergunta;
+        //}
+        [HttpPost]
+        //[Route("")]
+        public IActionResult Post(Pergunta pergunta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Perguntas.Add(pergunta);
+                _context.SaveChanges();
+                return Created($"/api/perguntas/{pergunta.Id}", pergunta);
+            }
+            return BadRequest(ModelState);
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Put(int id, [FromBody] Pergunta pergunta)
+        {
+            if (ModelState.IsValid)
+            {
+                //_context.Attach(pergunta);
+                _context.Perguntas.Attach(pergunta);
+                _context.Entry(pergunta).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+                _context.SaveChanges();
+                return Created($"/api/perguntas/{pergunta.Id}", pergunta);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
--- fp_stack.api/Startup.cs
+++ fp_stack.api/Startup.cs
@@ -3,37 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using fp_stack.core.Models;
-using fp_stack.core.Services;
-using fp_stack.web.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace fp_stack.web
+namespace fp_stack.api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<NoticiasServices>();
-
             var connection = @"Server=(localdb)\mssqllocaldb;Database=StackDB;Trusted_Connection=True;ConnectRetryCount=0";
             services.AddDbContext<Context>(options => options.UseSqlServer(connection));
-
-            services.AddMvc();
+            services.AddMvc( options =>
+                {
+                    options.RespectBrowserAcceptHeader = true;
+                    options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,14 +35,7 @@ namespace fp_stack.web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMeuLog();
-            app.UseStaticFiles();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
